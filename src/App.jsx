c2f8f07b6917000188561f322a1f400fb78a9e3c
Tharu <144349228+tharuka-14@users.noreply.github.com>
2025-07-29 14:48:42 +0530

    count is changed to 1000

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is 1000000000000000
+          count is 1000
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
